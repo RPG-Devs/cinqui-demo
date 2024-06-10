5c29855d4ffcdb9b6a4d9cd08351c437fd9df016
Renato <renatomercadoluna@hotmail.com>
2024-06-10 08:54:23 -0500

    event logger

--- src/components/EventLogger.svelte
+++ src/components/EventLogger.svelte
@@ -1,7 +1,4 @@
----
-import TitleHeader from './TitleHeader.astro';
-import type { MessageLog } from '~/types';
-import InputText from './InputText.astro';
+<script>
 
 // document
 // 	.getElementById('agregar')
@@ -12,7 +9,7 @@ import InputText from './InputText.astro';
 // 		textoImpreso.innerText = texto;
 // 	});
 
-const loadData: Array<MessageLog> = [
+export let loadData = [
     {
         message: 'El incidente fue reportado por Roy Soto.',
         time: '08:40 a.m',
@@ -40,28 +37,46 @@ const loadData: Array<MessageLog> = [
         time: '11:20 a.m',
     },
 ];
----
+
+// add new item to loadData
+function addData() {
+    const newMessage = document.getElementById('new_event').value;
+    const newTime = new Date().toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+
+    loadData =  [...loadData, { message: newMessage, time: newTime }];
+    document.getElementById('new_event').value = '';
+};
+
+</script>
 
 <section class="flex-col h-screen space-y-4 fixed">
-    <TitleHeader title="Eventos del incidente" />
     <div
         id="textoImpreso"
         class="border-4 border-gray p-5 rounded-lg min-h-3/4"
     >
         <ul class="flex-col space-y-4">
-            {
-                loadData.map((data) => (
+            {#each loadData as data} 
                     <li class="flex space-x-4">
                         <p class="whitespace-nowrap">{data.time}</p>
                         <p class="">{data.message}</p>
                     </li>
-                ))
-            }
+            {/each}
         </ul>
+    </div>    
+    <div class="border-4 border-gray p-4 rounded-lg">
+        <textarea
+        id="new_event"
+            rows="5"
+            class="shadow appearance-none border w-full py-2 px-3
+        leading-tight focus:outline-none focus:shadow-outline border-gray p-5 rounded-lg"
+            placeholder="Estación de almacen..."></textarea>
     </div>
-    <InputText />
     <div class="mb-4">
         <button
+            on:click={addData}
             id="agregar"
             type="button"
             class="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -71,3 +86,4 @@ const loadData: Array<MessageLog> = [
         </button>
     </div>
 </section>
+
