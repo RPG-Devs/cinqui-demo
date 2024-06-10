6f46d1f6eee66079b092ec1609f96a979deb3860
Renato <renatomercadoluna@hotmail.com>
2024-06-09 20:27:03 -0500

    update and new pages

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface RiskClassification {
+    src: string;
+    title: string;
+    unCodes: Array<string>;
+}
+
+export interface Employee {
+    name: string;
+    charge: string;
+    identification: string;
+}
+
+export interface Equipment {
+    name: string;
+    status: boolean;
+}
+
+export interface MessageLog {
+    user?: string;
+    message: string;
+    time: string;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        'primary': '#000000',
+        'accent': '#ED1C24',
+        'gray': '#D9D9D9'
+      },
+    },
   },
   plugins: [],
 }
