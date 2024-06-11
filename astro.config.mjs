111afa3d3493a068a213bcf985cc07b291cd9cd2
Renato <renatomercadoluna@hotmail.com>
2024-06-11 14:06:07 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   site: "https://rpg-devs.github.io",
   base: "/cinqui-demo",
-  output: 'static',
   integrations: [tailwind(), svelte()]
 });
