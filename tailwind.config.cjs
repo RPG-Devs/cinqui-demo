89aea5d2edac7b83190ffb4a74b6cc3803302d7a
Renato <renatomercadoluna@hotmail.com>
2024-06-11 09:03:54 -0500

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+			  },
 			colors: {
 				'primary': '#000000',
 				'accent': '#ED1C24',
