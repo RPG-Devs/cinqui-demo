f13d082f4ab56a92916984c54c3e0844604642a5
Renato <renatomercadoluna@hotmail.com>
2024-06-13 14:56:42 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
 
+const localhost_url = "http://localhost:4321";
+const github_url = "https://rpg-devs.github.io/cinqui-demo";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rpg-devs.github.io",
-  base: "/cinqui-demo",
+  site: "https://rpg-devs.github.io/cinqui-demo",
+  //base: process.env.BASE_URL  || "",
+  //base: '/',
   integrations: [tailwind(), svelte()]
 });
